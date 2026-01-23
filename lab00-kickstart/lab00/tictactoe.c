ed778b6952d815972c2d55119b5d8324b9f3fa7e
Nara539143 <nara.ureta@mi.unc.edu.ar>
2026-01-23 07:33:52 -0300

    Archivo tictactoe completo pero con errores

--- lab00-kickstart/lab00/tictactoe.c
+++ lab00-kickstart/lab00/tictactoe.c
@@ -6,15 +6,18 @@
 
 #define CELL_MAX (3 * 3 - 1)
 
+
+//me parece que esta esta mal, le faltan los corchetes al for, la voy a corregir
+
 void print_sep(int length) {
     printf("\t ");
-    for (int i=0; i < length;i++) printf("................");
+    for (int i=0; i < length;i++){
+        printf("................");
+    }
     printf("\n");
-
 }
 
-void print_board(char board[3][3])
-{
+void print_board(char board[3][3]){
     int cell = 0;
 
     print_sep(3);
@@ -28,21 +31,75 @@ void print_board(char board[3][3])
     }
 }
 
-char get_winner(char board[3][3])
-{
+char get_winner(char board[3][3]){
+
     char winner = '-';
-    //
-    // TODO: COMPLETAR
-    //
+    
+    bool winnerX = false;
+    bool winnerO = false;
+
+    //ganador por fila
+    int row=0;
+    while(row<3 && (!(winnerX)) && (!(winnerO))){
+        winnerX = true; //le asigno true por cada fila, y despues en el ciclo de abajo me 
+                        // fijo que cumpla para todos
+        winnerO = true;
+
+        for (int column=0; column<3; ++column){
+            winnerX=(board[row][column] = 'X') && winnerX;
+            winnerO=(board[row][column] = 'O') && winnerO;
+        }
+        ++row;
+        printf("PRIMER CICLO");
+    }
+
+    //ganador por columna
+    int column=0;
+    while(column<3 && (!(winnerX)) && (!(winnerO))){
+        winnerX=true;
+        winnerO=true;
+
+        for(int row=0; row<3; row++){
+            winnerX=(board[row][column] = 'X') && winnerX;
+            winnerO=(board[row][column] = 'O') && winnerO;
+        }
+        ++column;
+        printf("SEGUNDO CICLO");
+    }
+
+    //ganador por diagonal descendente
+    if((!(winnerX)) && (!(winnerO))){
+        for(int row_column=0; row_column<3; ++row_column){
+            winnerX=(board[row_column][row_column] = 'X') && winnerX;
+            winnerO=(board[row_column][row_column] = 'O') && winnerO;
+        }
+    }
+
+    //ganador diagonal ascendente
+    if((!(winnerX)) && (!(winnerO))){
+        for (int row_column=0; row_column<3; ++row_column){
+            winnerX=(board[2-row_column][row_column] = 'X') && winnerX;
+            winnerO=(board[2-row_column][row_column] = 'O') && winnerO;
+        }
+    }
+
+    if(winnerX){
+        winner= 'X';
+    } else{
+        winner = 'O';
+    };
+
     return winner;
 }
 
 bool has_free_cell(char board[3][3])
 {
     bool free_cell=false;
-    //
-    // TODO: COMPLETAR
-    //
+    for(int row = 0; row<3; ++row){
+        for(int column = 0; column<3; ++column){
+            free_cell = free_cell || (board[row][column] = '-');
+        }
+    }
     return free_cell;
 }
 
