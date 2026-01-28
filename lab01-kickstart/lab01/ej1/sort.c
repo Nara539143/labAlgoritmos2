8cbbf2faac0ba4f71928e30f8a14853acf625e27
Nara539143 <nara.ureta@mi.unc.edu.ar>
2026-01-28 11:08:08 -0300

    Ej1: codigo completo del insertion

--- lab01-kickstart/lab01/ej1/sort.c
+++ lab01-kickstart/lab01/ej1/sort.c
@@ -6,9 +6,23 @@
 #include "sort_helpers.h"
 #include "sort.h"
 
+/*
+proc insert (in/out a:array[1..n] of T, in i: nat)
+	var j:nat
+	j:=i
+	do j>1 ∧ a[j]<a[j-1] → 
+		swap(a,j-1,j)
+		j:=j-1
+	od
+end proc
+*/
 
 static void insert(int a[], unsigned int i, unsigned int length) {
-    /* needs implementation */
+    unsigned int j=i;
+    while((j>0) && (a[j]<a[j-1])){
+        swap(a,j-1,j);
+        --j;
+    }
 }
 
 void insertion_sort(int a[], unsigned int length) {
