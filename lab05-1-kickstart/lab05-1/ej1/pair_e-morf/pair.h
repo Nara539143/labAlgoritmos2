441700da0cfa0e9a14cecd7bcee718f4cdfa9f6b
Nara539143 <nara.ureta@mi.unc.edu.ar>
2026-02-14 15:41:45 -0300

    ejercicio e completo

--- lab05-1-kickstart/lab05-1/ej1/pair_e-morf/pair.h
+++ lab05-1-kickstart/lab05-1/ej1/pair_e-morf/pair.h
@@ -2,7 +2,7 @@
 #define _PAIR_H
 
 typedef struct s_pair_t * pair_t;
-typedef int elem;
+typedef int elem; //ES MUY BUENA, puedo cambiar el tipo de todo desde aca
 
 pair_t pair_new(elem x, elem y);
 /*
